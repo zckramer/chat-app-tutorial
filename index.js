27809c4651c358ab3ad26c040ee0642e55b503ea
Zachary Kramer <zckramer83@gmail.com>
2020-04-10 21:18:33 -0400

    added basic socket chat

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('we got a user here')
+    socket.on('disconnect', function(){
+        console.log('we got a user leavin here')
+    })
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);
+    })
 });
 
 http.listen(3000, function(){
